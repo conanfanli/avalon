516b1862240682159e944b9bfe376fad9e087c98
Conan Li <conanlics@gmail.com>
2017-10-19 00:39:39 +0000

    filled in

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,18 +1,19 @@
 
 // Libs
 import * as React from 'react'
-// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-// import { Provider } from 'react-redux'
-// import {Route} from 'react-router-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { Provider } from 'react-redux'
+import {Route} from 'react-router-dom'
 // import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
-// import { ConnectedRouter } from 'react-router-redux'
+import { ConnectedRouter } from 'react-router-redux'
 
 class AppContainer extends React.Component<any, any> {
 
     render() {
         return (
             <div>
+                This is my app
             </div>
         )
     }
@@ -29,21 +30,17 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-export const Root = connect(
+export const AppComponent = connect(
     mapStateToProps,
     mapDispatchToProps
 )(AppContainer)
 
 export const App = ({store, history}) => {
-    return <div></div>
-}
-
-    /*
     return <Provider store={store}>
         <MuiThemeProvider>
             <ConnectedRouter history={history}>
-                <Route path="/" component={Root}/>
+                <Route path="/" component={AppComponent}/>
             </ConnectedRouter>
         </MuiThemeProvider>
     </Provider>
-     */
+}
